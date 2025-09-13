63ab38a75a9f84bd926fd3b0edb1151ef13199b6
Bjoern Boss Henrichsen <bjoernbossdev@gmail.com>
2025-09-13 13:54:30 +0200

    Add initial main crossword game selection view

--- handler/crossword/app.js
+++ handler/crossword/app.js
@@ -15,9 +15,19 @@ export const SubPath = '/crossword';
 
 export function Handle(msg) {
 	libLog.Log(`Game handler for [${msg.relative}]`);
-
-	/* respond to the request by trying to server the file */
 	if (!msg.ensureMethod(['GET']))
 		return;
+
+	/* check if its a redirection and forward it accordingly */
+	if (msg.relative == '/' || msg.relative == '/main') {
+		msg.respondFile(fileRelative('static/main.html'), false);
+		return;
+	}
+	if (msg.relative == '/editor') {
+		msg.respondFile(fileRelative('static/editor.html'), false);
+		return;
+	}
+
+	/* respond to the request by trying to server the file */
 	msg.tryRespondFile(fileRelative('static' + msg.relative), false);
 }
